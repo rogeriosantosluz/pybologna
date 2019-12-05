7cc6666d22fe117a70a9836b1b4f1e7892da1b31
Rogerio Luz <rogerioluz@MacBook-Pro-de-Rogerio.local>
2019-12-05 19:47:20 -0300

    Test run

--- test_game.py
+++ test_game.py
@@ -15,10 +15,10 @@ class TestGame(unittest.TestCase):
         random_gender = ['M','F']
         self.game = Game()
         game_level = random.randint(1,4)
-        print "Game Level " , str(game_level)
+        print("Game Level {}".format(game_level))
         self.game.choose_level(game_level)
         players_number = random.randint(1,4)
-        print "Players Number " , str(players_number)
+        print("Players Number {}".format(players_number))
         self.game.players_number(players_number)
         for reg in range (0,self.game.players_num):
             self.game.choose_player(reg, random_names[random.randint(0,len(random_names)-1)], random_gender[random.randint(0,len(random_gender)-1)])
@@ -101,11 +101,11 @@ class TestGame(unittest.TestCase):
     Should print game results but dont know why it doesnt print
     """
     def test_final_results(self):
-        print "#########################"
-        print "Game:", self.game.__dict__
+        print("#########################")
+        print("Game: {}".format(self.game.__dict__))
         for player in self.game.players:
-            print "Player:", player.__dict__
-        print "#########################"
+            print("Player: {}".format(player.__dict__))
+        print("#########################")
 
 """
 Comment lines below to test the game
